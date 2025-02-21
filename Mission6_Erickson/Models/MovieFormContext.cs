90d321a222aedd06de995114e1ee5e44cd16829c
Madi Erickson <emadisom@byu.edu>
2025-02-21 14:47:26 -0700

    SAVE

--- Mission6_Erickson/Models/MovieFormContext.cs
+++ Mission6_Erickson/Models/MovieFormContext.cs
@@ -4,25 +4,24 @@ namespace Mission6_Erickson.Models
 {
     public class MovieFormContext : DbContext
     {
-        public MovieFormContext(DbContextOptions<MovieFormContext> options) : base (options)
+        public MovieFormContext(DbContextOptions<MovieFormContext> options) : base(options)
         {
         }
 
-        public DbSet<Movie> Forms { get; set; }
+        public DbSet<Movie> Movies { get; set; }  // Rename from "Forms" to "Movies"
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                
-              new Category { CategoryID=1, CategoryName="Action"}, 
-              new Category { CategoryID=2, CategoryName="Romantic Comedy"},
-              new Category { CategoryID=3, CategoryName = "Horror" },
-              new Category { CategoryID=4, CategoryName = "Drama" },
-              new Category { CategoryID=5, CategoryName = "Television" },
-              new Category { CategoryID=6, CategoryName = "Comedy" },
-              new Category { CategoryID=7, CategoryName = "Family" },
-              new Category { CategoryID=8, CategoryName = "Miscellaneous" }
+                new Category { CategoryID = 1, CategoryName = "Action" },
+                new Category { CategoryID = 2, CategoryName = "Romantic Comedy" },
+                new Category { CategoryID = 3, CategoryName = "Horror" },
+                new Category { CategoryID = 4, CategoryName = "Drama" },
+                new Category { CategoryID = 5, CategoryName = "Television" },
+                new Category { CategoryID = 6, CategoryName = "Comedy" },
+                new Category { CategoryID = 7, CategoryName = "Family" },
+                new Category { CategoryID = 8, CategoryName = "Miscellaneous" }
             );
         }
     }
--- Mission6_Erickson/Program.cs
+++ Mission6_Erickson/Program.cs
@@ -6,10 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddDbContext<MovieFormContext>(options =>
+//{
+//    options.UseSqlite(builder.Configuration["ConnectionStrings:MovieConnection"]);
+//});
+
 builder.Services.AddDbContext<MovieFormContext>(options =>
-{
-    options.UseSqlite(builder.Configuration["ConnectionStrings:MovieConnection"]);
-});
+    options.UseSqlite(builder.Configuration.GetConnectionString("MovieConnection")));
 
 var app = builder.Build();
 
